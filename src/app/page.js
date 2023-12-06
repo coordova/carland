4f95657fdabb8c50ad22c9e89822fc98b18e1fd9
coordova <grcordova@gmail.com>
2023-12-06 18:58:38 -0400

    Carland - Header & Search

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
 	return (
 		<main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
 			<Header />
+			<div className="h-[4000px]"></div>
 			<Hero />
 			<Cars />
 			<About />
